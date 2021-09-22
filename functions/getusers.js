b440a004a0be815332473cd6e5254d8805f31c26
Abdul Rehman Khursheed <malik.ali5560@gmail.com>
2021-09-22 07:53:14 -0700

    newsssss

--- functions/getusers.js
+++ functions/getusers.js
@@ -1,13 +1,10 @@
 const axios = require("axios");
- 
+
 exports.handler = async function (event, context, callback) {
   const data = await axios("https://api.publicapis.org/entries");
-  
-  console.log(context, "Muhammad Ahsan");
   if (event.httpMethod == "GET") {
-  
-      callback(null, {
-      statusCode: 200, 
+    callback(null, {
+      statusCode: 200,
       body: JSON.stringify({
         msg: "Following is the api response from the public animal names apis ",
         data: data.data,
@@ -20,19 +17,17 @@ exports.handler = async function (event, context, callback) {
         msg: "This was a POST request",
       }),
     });
-  } else if(event.httpMethod == "PUT") {
+  } else if (event.httpMethod == "PUT") {
     callback(null, {
       statusCode: 500,
       body: "This was a  PUT request ",
     });
-  }
-  else if(event.httpMethod == "DELETE") {
+  } else if (event.httpMethod == "DELETE") {
     callback(null, {
       statusCode: 200,
       body: "This was new ones DELETE  request ",
     });
-  }
-  else  {
+  } else {
     callback(null, {
       statusCode: 500,
       body: "Some error occured in server.",
