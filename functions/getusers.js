bc30503ac5fbd79715143a38b688ab2f62114c15
Abdul Rehman Khursheed <malik.ali5560@gmail.com>
2021-09-22 07:18:12 -0700

    New one

--- functions/getusers.js
+++ functions/getusers.js
@@ -19,7 +19,7 @@ exports.handler = async function (event, context, callback) {
   } else {
     callback(null, {
       statusCode: 500,
-      body: "Some error occured. ",
+      body: "Some error occured with server.  ",
     });
   }
 };
